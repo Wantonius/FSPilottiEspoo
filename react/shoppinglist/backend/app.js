1a3bf07e2a3706e7b9dd4aa6f193ac1643c5ce87
Erno Hentonen <erno.hentonen@opiframe.com>
2018-11-20 09:42:03 +0200

    moved to express router

--- react/shoppinglist/backend/app.js
+++ react/shoppinglist/backend/app.js
@@ -1,49 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser")
+const shoppingrouter = require("./routes/shoppingrouter");
 
 let app = express();
 
 app.use(bodyParser.json());
 
-//"database"
-
-let database = [
-	{
-		"id":99,
-		"type":"Banana",
-		"count":2,
-		"price":3
-	}
-];
-let id = 100;
-
-app.get("/api/shopping", function(req,res) {
-	res.status(200).json(database);
-})
-
-app.post("/api/shopping", function(req,res) {
-	let item = {
-		id:id,
-		type:req.body.type,
-		count:req.body.count,
-		price:req.body.price
-	}
-	id++;
-	database.push(item);
-	console.log(database);
-	res.status(200).json({"message":"success"});
-});
-
-app.delete("/api/shopping/:id", function(req,res) {
-	let tempId = parseInt(req.params.id,10);
-	for(let i=0;i<database.length;i++) {
-		if(database[i].id === tempId ) {
-			database.splice(i,1);
-			return res.status(200).json({"message":"success"})
-		}
-	}
-	res.status(404).json({"message":"not found"});
-});
+app.use("/api",shoppingrouter);
 
 app.listen(3001);
 console.log("Running in port 3001")
