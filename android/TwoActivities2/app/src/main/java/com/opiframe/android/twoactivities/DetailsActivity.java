6f5ee69c73fb2c4e3d977b22568c782a0af0c85b
Erno Hentonen <erno.hentonen@opiframe.com>
2019-02-04 14:55:39 +0200

    location map example

--- android/TwoActivities2/app/src/main/java/com/opiframe/android/twoactivities/DetailsActivity.java
+++ android/TwoActivities2/app/src/main/java/com/opiframe/android/twoactivities/DetailsActivity.java
@@ -1,13 +1,37 @@
 package com.opiframe.android.twoactivities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private Button saveButton;
+    private EditText type,price,count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        type = findViewById(R.id.itemtype);
+        price = findViewById(R.id.itemprice);
+        count = findViewById(R.id.itemcount);
+        saveButton = findViewById(R.id.savebutton);
+        setResult(Activity.RESULT_CANCELED);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = getIntent();
+                i.putExtra("type",type.getText().toString());
+                i.putExtra("price",price.getText().toString());
+                i.putExtra("count",count.getText().toString());
+                setResult(Activity.RESULT_OK,i);
+                finish();
+            }
+        });
     }
 }
--- android/TwoActivities2/app/src/main/java/com/opiframe/android/twoactivities/MainActivity.java
+++ android/TwoActivities2/app/src/main/java/com/opiframe/android/twoactivities/MainActivity.java
@@ -1,13 +1,60 @@
 package com.opiframe.android.twoactivities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button detailsButton;
+    private Button otherButton;
+    private EditText longitude, latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        detailsButton = findViewById(R.id.detailsbutton);
+        otherButton = findViewById(R.id.showmebutton);
+        longitude = findViewById(R.id.longitude);
+        latitude = findViewById(R.id.latitude);
+        detailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, DetailsActivity.class);
+                startActivityForResult(i,100);
+            }
+        });
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String longi="0.0";
+                String lati="0.0";
+                longi = longitude.getText().toString();
+                lati = latitude.getText().toString();
+                String temp = String.format("geo:%s,%s?z=16",lati,longi);
+                Uri mapUri = Uri.parse(temp);
+                Intent i = new Intent();
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(mapUri);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 100) {
+            if(resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this,""+data.getStringExtra("type")+", price:"+data.getStringExtra("price")
+                        +", count:"+data.getStringExtra("count"),Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
